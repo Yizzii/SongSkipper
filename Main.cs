0dbe1271d107363086941dbb90bd107e1c085345
Yizzi <86291762+Yizzii@users.noreply.github.com>
2023-11-20 08:02:59 -0600

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,28 @@
+﻿using TLDLoader;
+using UnityEngine.InputSystem;
+namespace SongSkipper
+{
+	public class Main : Mod
+	{
+		public override string ID { get { return "Song Skipper"; } }
+		public override string Name { get { return "Song Skipper"; } }
+		public override string Author { get { return "Yizzi"; } }
+		public override string Version { get { return "1.0.0"; } }
+		bool keyp;
+        public override void Update()
+        {
+            if (Keyboard.current.equalsKey.isPressed)
+            {
+                if (!keyp)
+                {
+                    mainscript.M.customRadio.LoadRandomSong();
+                }
+                keyp = true;
+            }
+            else
+            {
+                keyp = false;
+            }
+        }
+    }
+}
